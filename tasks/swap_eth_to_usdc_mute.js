2885933d87a3ca9895377be5f60995a513be439d
Vishesh Sachdev <visheshsachdev04@gmail.com>
2024-02-09 10:43:31 +0530

    minor fixes

--- tasks/swap_eth_to_usdc_mute.js
+++ tasks/swap_eth_to_usdc_mute.js
@@ -12,10 +12,16 @@ task("swap_eth_to_usdc_mute", async (taskArgs, hre) => {
     const ROUTER = "0x8B791913eB07C32779a16750e3868aA8495F5964"
     const weth = new hre.ethers.Contract(WETH, erc20, signer.provider);
     const data = await read('txdata.json');
-    const ethToUse = data.ethReceivedOnZksyncEra;
+    let ethToUse = data.ethReceivedOnZksyncEra;
+    if (parseFloat(ethToUse) > 0.012) {
+        ethToUse = (parseFloat(ethToUse) - 0.004).toFixed(18) // Ensuring precision suitable for Ethereum
+    } else {
+        ethToUse = (parseFloat(ethToUse) - 0.002).toFixed(18); // Adjusting and ensuring precision
+    }
+
     console.log("Converting ETH to WETH");
     await weth.connect(signer).deposit({
-        value: hre.ethers.parseEther(ethToUse.toString())
+        value: hre.ethers.parseEther(ethToUse)
     })
     console.log("Successfully Converted ETH to WETH");
     console.log("Approving WETH to MUTE pool for swap");
